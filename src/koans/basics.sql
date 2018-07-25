d2fa40880ef79ab0ea48326c611d9b972ba93702
sakshi aggarwal <sakshi123@gmail.com>
2018-07-25 23:57:19 +0530

    commit

--- src/koans/basics.sql
+++ src/koans/basics.sql
@@ -2,16 +2,16 @@
 SELECT 1
 
 -- Meditate on lower-case queries
-_____ 1
+select 1
 
 -- Meditate on selecting all columns from a table
 select * from book
 
 -- Meditate on selecting one column (title) from a table
-select _____ from book
+select title from book
 
 -- Meditate on selecting some columns (title, available) from a table
-select _____, _____ from book
+select title, available from book
 
 -- Meditate on adding a new book
 insert into book(title, genre, year_published, available)
@@ -19,5 +19,5 @@ values('_____', '_____', _____, 1)
 
 -- Meditate on adding two new books
 insert into book(title, genre, year_published, available)
-values('_____', '_____', _____, 1)
-,('_____', '_____', _____, 1)
+values('ABC', 'fantasy',2001, 1)
+,('XYZ', 'fantasy', 2002, 1)
